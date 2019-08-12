e7412e9df387b36b59cd783d12e3f393dc04ba0f
hellotimwebber <33205687+hellotimwebber@users.noreply.github.com>
2019-08-12 11:33:04 -0500

    Update caucus-bot.py
    Updated script to pull Twitter credentials

--- caucus-bot.py
+++ caucus-bot.py
@@ -4,6 +4,12 @@ import json
 import urllib.request
 import time
 from datetime import datetime
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
